c59253f39b83454c1f92dec622433695731fe8de
patpatia <kartik@iimjobs.com>
2017-04-04 17:42:09 +0530

    mail system

--- project/__init__.py
+++ project/__init__.py
@@ -1,11 +1,14 @@
 from flask import Flask
 
 app = Flask(__name__)
-
 app.secret_key ='sdlkfdklsjfljsd'
+
+app.config['CELERY_BROKER_URL'] = 'redis://localhost:6379/0'
+app.config['CELERY_RESULT_BACKEND'] = 'redis://localhost:6379/0'
 
 from users.views import users
 from jobListing.views import listingJob
+
 
 
 app.register_blueprint(users)
--- project/users/views.py
+++ project/users/views.py
@@ -2,14 +2,20 @@ from flask import render_template, Blueprint,request,redirect,url_for,session
 from pymongo import MongoClient
 from ..utilities.commonFunctions import Utility
 from ..DBHelper.dbhelper import Dbhelper
+from celery import Celery
+from flask import Flask
 
 import smtplib
 from email.MIMEMultipart import MIMEMultipart
 from email.MIMEText import MIMEText
 from random import randint
-
+app = Flask(__name__)
 users = Blueprint('users', __name__, template_folder='templates')
+app.config['CELERY_BROKER_URL'] = 'redis://localhost:6379/0'
+celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
+celery.conf.update(app.config)
 
+@celery.task
 def sendMail(fromaddr,password,toaddr,body):
 	msg = MIMEMultipart()
 	msg['From'] = fromaddr
@@ -58,11 +64,11 @@ def login():
 
 				if session['lis']:
 				   return redirect(url_for('jobListing.jobListing'))
-				else:   
+				else:
 				   return render_template('companiesPopUp.html')
 		else:
 			return render_template('login.html',message="Enter valid credential")
-			
+
 @users.route('/admin')
 def admin():
 	if Utility.isLoggedIn():
@@ -79,7 +85,7 @@ def admin():
 def logout():
 	session.clear()
 	return render_template('login.html',logoutMessage="You have successfully logged out")
-			
+
 @users.route('/register')
 def register():
 	return render_template('register.html')
@@ -97,8 +103,8 @@ def registerSucess():
 			sendMail("mohit.mittal@iimjobs.com","123456789!@",email,body)
 			return redirect(url_for('users.admin'))
 		else:
-			return render_template('register.html',message="Emailid is already registered")	
-	return redirect(url_for('users.index'))	
+			return render_template('register.html',message="Emailid is already registered")
+	return redirect(url_for('users.index'))
 
 @users.route('/admin/delete/<emailId>')
 def delete(emailId):
@@ -113,8 +119,4 @@ def selectCompanies():
 		collection=Dbhelper.getCollectionName('User')
 		collection.update({"email":session['email']},{"$set":{"permission":{"crawler":listOfCompanies}}})
 		session['lis'] = listOfCompanies;
-		return redirect(url_for('jobListing.jobListing'))		
-	
-    
-
-		
+		return redirect(url_for('jobListing.jobListing'))
