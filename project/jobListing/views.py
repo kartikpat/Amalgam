72d70f061d60f2d1f2bf84c679c82bea5c38443a
MohitMittal188 <mohitmittal188@gmail.com>
2017-04-26 15:44:06 +0530

    change password validation

--- project/jobListing/views.py
+++ project/jobListing/views.py
@@ -8,53 +8,55 @@ listingJob = Blueprint('jobListing',__name__,template_folder='templates')
 
 @listingJob.route('/logout')
 def logout():
-	session.clear()
-	return redirect(url_for('users.login'))
+    session.clear()
+    return redirect(url_for('users.login'))
 
 @listingJob.route('/editProfile', methods = ['POST', 'GET'])
 def editProfile():
-	if request.method == 'POST':
-		collection = Dbhelper.getCollectionName("User")
-		oldPassword = Utility.getPostParameter('oldPassword')
-		newPassword = Utility.getPostParameter('newPassword')
-		if oldPassword and newPassword:
-			collection.update({"email":session['email'],"password":oldPassword},{"$set":{"password":newPassword}})
-
-		return redirect(url_for('jobListing.logout'))
+    if request.method == 'POST':
+        collection = Dbhelper.getCollectionName("User")
+        oldPassword = Utility.getPostParameter('oldPassword')
+        newPassword = Utility.getPostParameter('newPassword')
+        if Dbhelper.findOne('User',{"email":session['email'],"password":oldPassword}):
+            collection.update({"email":session['email'],"password":oldPassword},{"$set":{"password":newPassword}})
+            return redirect(url_for('jobListing.logout'))
+        else:   
+            output = model.getRecentJobs(session['lis'])
+            return render_template('index.html',wrongPassword="Enter correct old password",dict=output,selectedList=session['lis'],list=JobsModel.allCmnyList)
 
 @listingJob.route('/editCompanies')
 def editCompanies():
-	collection=Dbhelper.getCollectionName("User")
-	companies=request.args.getlist('permissionCompanies')
-	if companies:
-		collection.update({"email":session['email']},{"$set":{"permission.crawler":companies}})
-		session['lis']=companies
+    collection=Dbhelper.getCollectionName("User")
+    companies=request.args.getlist('permissionCompanies')
+    if companies:
+        collection.update({"email":session['email']},{"$set":{"permission.crawler":companies}})
+        session['lis']=companies
 
-	return redirect(url_for('jobListing.jobListing'))
+    return redirect(url_for('jobListing.jobListing'))
 
 @listingJob.route('/jobListing')
 def jobListing():
-	if Utility.isLoggedIn():
-		output = model.getRecentJobs(session['lis'])
+    if Utility.isLoggedIn():
+        output = model.getRecentJobs(session['lis'])
 
-		return render_template("index.html",dict=output,selectedList=session['lis'],list=JobsModel.allCmnyList )
-	else:
-		return redirect(url_for('users.index'))
+        return render_template("index.html",dict=output,selectedList=session['lis'],list=JobsModel.allCmnyList )
+    else:
+        return redirect(url_for('users.index'))
 
 @listingJob.route('/jobListing/listJob')
 def listJob():
 
-	cmpnyName = Utility.getUrlParameter('company_name')
-	startDate = Utility.getUrlParameter('start_date')
-	endDate = Utility.getUrlParameter('end_date')
+    cmpnyName = Utility.getUrlParameter('company_name')
+    startDate = Utility.getUrlParameter('start_date')
+    endDate = Utility.getUrlParameter('end_date')
 
-	listOfJobs = JobsModel(startDate,endDate,cmpnyName)
+    listOfJobs = JobsModel(startDate,endDate,cmpnyName)
 
-	if startDate and endDate and cmpnyName:
-		return listOfJobs.datecmnywiseJob()
-	elif startDate and endDate:
-		return listOfJobs.datewiseJob()
-	elif cmpnyName:
-		return listOfJobs.cmnywiseJob()
-	else:
-		return redirect(url_for('jobListing.jobListing'))
+    if startDate and endDate and cmpnyName:
+        return listOfJobs.datecmnywiseJob()
+    elif startDate and endDate:
+        return listOfJobs.datewiseJob()
+    elif cmpnyName:
+        return listOfJobs.cmnywiseJob()
+    else:
+        return redirect(url_for('jobListing.jobListing'))
